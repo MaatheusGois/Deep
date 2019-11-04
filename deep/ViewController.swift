5a8b926f8d23e1ec8f15969c2a8cc4f30af8de9e
Gois <matheusgoislimasilva@gmail.com>
2019-11-04 03:30:09 -0300

    first

--- deep/ViewController.swift
+++ deep/ViewController.swift
@@ -9,10 +9,12 @@
 import UIKit
 import SceneKit
 import ARKit
+import QuartzCore
 
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    let scene = SCNScene(named: "art.scnassets/scene.scn")!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,7 +26,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        
+        configBlackHole()
         
         // Set the scene to the view
         sceneView.scene = scene
@@ -46,6 +49,25 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
+    
+    func configBlackHole()
+    {
+        guard let holeScene = SCNScene(named: "art.scnassets/blackhole.scn") else { return }
+        
+        guard let blackhole_ring = holeScene.rootNode.childNode(withName: "Blackhole_ring", recursively: false) else { return }
+        
+        guard let blackhole_core = holeScene.rootNode.childNode(withName: "Blackhole_core", recursively: false) else { return }
+        
+        scene.rootNode.addChildNode(blackhole_ring)
+        scene.rootNode.addChildNode(blackhole_core)
+        
+        for i in 1...3 {
+            let str = "Blackhole_skin_0\(i)"
+            guard let skin = holeScene.rootNode.childNode(withName: str, recursively: false) else { return }
+            scene.rootNode.addChildNode(skin)
+        }
+    }
+    
 
     // MARK: - ARSCNViewDelegate
     
@@ -71,5 +93,40 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     func sessionInterruptionEnded(_ session: ARSession) {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
         
+    }
+    @objc
+    func handleClick(_ gestureRecognizer: UIGestureRecognizer) {
+        // retrieve the SCNView
+        guard let scnView = self.view as? SCNView else { return }
+        
+        // check what nodes are clicked
+        let p = gestureRecognizer.location(in: scnView)
+        let hitResults = scnView.hitTest(p, options: [:])
+        // check that we clicked on at least one object
+        if hitResults.count > 0 {
+            // retrieved the first clicked object
+            let result = hitResults[0]
+            
+            // get its material
+            guard let material = result.node.geometry?.firstMaterial else { return }
+            
+            // highlight it
+            SCNTransaction.begin()
+            SCNTransaction.animationDuration = 0.5
+            
+            // on completion - unhighlight
+            SCNTransaction.completionBlock = {
+                SCNTransaction.begin()
+                SCNTransaction.animationDuration = 0.5
+                
+                material.emission.contents = UIColor.black
+                
+                SCNTransaction.commit()
+            }
+            
+            material.emission.contents = UIColor.red
+            
+            SCNTransaction.commit()
+        }
     }
 }
